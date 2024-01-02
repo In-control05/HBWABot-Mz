a856a54eac10a646e05cad24942b581ae4197f06
Yatoplays <154549954+Yatoplays@users.noreply.github.com>
2024-01-03 01:10:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ const fs = require("fs")
 
 //aumto presence update
 global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
-global.GroupWcMsg = false // false a i dah chuan welcome message i off ang
-global.autoTyping = true // true a i dah chuan i type la a lang ang
-global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
+global.GroupWcMsg = true // false a i dah chuan welcome message i off ang
+global.autoTyping = false // true a i dah chuan i type la a lang ang
+global.autoRecord = true // true a i dah chuan Voice record angin i lang ang
 global.available = true // false a i dah chuan offline angin a awm ang
 global.autoread = true // true a i dah chuan message zawng zawng read vek zelin a awm ang 
 global.autoread_status = true // true a i dah chuan auto status view in a awm ang
@@ -17,7 +17,7 @@ global.ownername = "Lalmesaka" // I hming dah tel bawk la
 global.location = "dinthar, Mizoram, Aizawl" // I awmna hmun daha
 global.botname = "HBWABot Mizo" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
-global.themeemoji = '🤖'
+global.themeemoji = '😌'
 global.creator ="918256931545@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
 global.ownerNumber = ["919863840872@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
